7eaaf68bf2291c59806dfa5e968967fbb4c69ada
tal <tal@9b05a68b-5939-0410-bc0c-8e10e763eb1c>
2007-10-13 10:51:27 +0000

    Making the test window do more stuff.

--- TestApp/DefconAppKitTest.py
+++ TestApp/DefconAppKitTest.py
@@ -30,50 +30,56 @@ class DefconAppKitTestDocumentWindow(object):
         self.font = font
         self.glyphs = [font[k] for k in sorted(font.keys())]
         self.w = vanilla.Window((500, 500), minSize=(300, 100))
-        self.w.glyphList = vanilla.List((0, 0, 200, -70), sorted(font.keys()), selectionCallback=self.selection)
-        self.w.modifyButton = vanilla.Button((10, -60, 180, 20), "Modify Glyphs", callback=self.modify)
-        self.w.sizeSlider = vanilla.Slider((10, -30, 180, 20), minValue=10, maxValue=100, value=50,
-            continuous=False, callback=self.resize)
-        self.w.cellView = GlyphCellView((200, 0, -0, -0),
-            selectionCallback=self.selectionCallback, doubleClickCallback=self.doubleClickCallback,
-            deleteCallback=self.deleteCallback, dropCallback=self.dropCallback)
-        self.w.cellView.set(self.glyphs)
-        self.resize(self.w.sizeSlider)
+
+        self.w.tabs = vanilla.Tabs((10, 10, -10, -10), ["Window", "GlyphCellView"])
+        self.windowTab = self.w.tabs[0]
+        self.cellViewTab = self.w.tabs[1]
+
+        # test automatic updating
+        self.cellViewTab.cellViewModifyButton = vanilla.Button((10, 10, 150, 20), "Modify Glyphs", callback=self.cellViewModify)
+        # test cell sizes
+        self.cellViewTab.cellViewSizeSlider = vanilla.Slider((170, 10, 150, 20), minValue=10, maxValue=100, value=50,
+            continuous=False, callback=self.cellViewResize)
+        # the cell view
+        self.cellViewTab.cellView = GlyphCellView((10, 40, -10, -10),
+            selectionCallback=self.cellViewSelectionCallback, doubleClickCallback=self.cellViewDoubleClickCallback,
+            deleteCallback=self.cellViewDeleteCallback, dropCallback=self.cellViewDropCallback)
+        self.cellViewTab.cellView.set(self.glyphs)
+        self.cellViewResize(self.cellViewTab.cellViewSizeSlider)
+
         self.w.open()
 
-    def selectionCallback(self, sender):
-        print "selection", sender.getSelection()
+    # cell view
 
-    def doubleClickCallback(self, sender):
+    def cellViewDoubleClickCallback(self, sender):
         print "double click"
 
-    def deleteCallback(self, sender):
+    def cellViewDeleteCallback(self, sender):
         print "delete", sender.getSelection()
 
-    def dropCallback(self, sender, glyphs, testing):
+    def cellViewDropCallback(self, sender, glyphs, testing):
         if not testing:
             for glyph in glyphs:
                 self.font.insertGlyph(glyph, name=".glyph%d" % len(self.font))
             self.glyphs = [self.font[k] for k in sorted(self.font.keys())]
-            self.w.cellView.set(self.glyphs)
-            self.w.glyphList.set(sorted(self.font.keys()))
+            self.cellViewTab.cellView.set(self.glyphs)
         return True
 
-    def selection(self, sender):
-        self.w.cellView.setSelection(sender.getSelection())
+    def cellViewSelectionCallback(self, sender):
+        self.cellViewTab.cellView.setSelection(sender.getSelection())
 
-    def modify(self, sender):
-        selection = [self.glyphs[index] for index in self.w.cellView.getSelection()]
+    def cellViewModify(self, sender):
+        selection = [self.glyphs[index] for index in self.cellViewTab.cellView.getSelection()]
         for glyph in selection:
-            glyph.move((50, 50))
+            glyph.move((100, 100))
 
-    def resize(self, sender):
+    def cellViewResize(self, sender):
         width = height = int(sender.get())
         drawHeader = height > GlyphCellMinHeightForHeader
         if drawHeader:
             height += GlyphCellHeaderHeight
-        self.w.cellView.setCellSize((width, height))
-        self.w.cellView.setRepresentationArguments(drawHeader=drawHeader, drawMetrics=drawHeader)
+        self.cellViewTab.cellView.setCellSize((width, height))
+        self.cellViewTab.cellView.setRepresentationArguments(drawHeader=drawHeader, drawMetrics=drawHeader)
 
 
 if __name__ == "__main__":
