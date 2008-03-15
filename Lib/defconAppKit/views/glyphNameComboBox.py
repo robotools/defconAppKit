42ab9538b186a68e25a45e304bd7cc3eb3b7c89a
tal <tal@9b05a68b-5939-0410-bc0c-8e10e763eb1c>
2008-03-15 22:29:10 +0000

    Smarter about deleting.

--- Lib/defconAppKit/views/glyphNameComboBox.py
+++ Lib/defconAppKit/views/glyphNameComboBox.py
@@ -22,7 +22,7 @@ class GlyphNameComboBox(vanilla.EditText):
             if self._currentText.startswith(input):
                 deleting = True
         self._currentText = input
-        input, match = self._searchForMatch(input, deleting=deleting)
+        input, match = _search(input, self._font, deleting=deleting)
         if match is None:
             return
         if match != input:
@@ -37,34 +37,65 @@ class GlyphNameComboBox(vanilla.EditText):
         if self._finalCallback is not None:
             self._finalCallback(self)
 
-    def _searchForMatch(self, text, deleting=False):
-        # no text
-        if not text:
-            return text, None
-        glyphNames = self._font.keys()
-        match = None
-        # direct match
-        if text in glyphNames:
-            match = text
-        # character entry
-        elif len(text) == 1:
-            uniValue = ord(text)
-            match = self._font.unicodeData.glyphNameForUnicode(uniValue)
-            if match is not None:
-                text = ""
-        # fallback. find closest match
-        if match is None:
-            glyphNames = list(sorted(glyphNames))
-            if not deleting:
-                for glyphName in glyphNames:
-                    if glyphName.startswith(text):
-                        match = glyphName
-                        break
-            else:
-                for glyphName in glyphNames:
-                    if text.startswith(glyphName):
-                        match = glyphName
-                    elif match is not None:
-                        break
-        return text, match
 
+def _search(text, font, deleting):
+    """
+    >>> from fontTools.agl import AGL2UV
+    >>> from defcon import Font
+    >>> font = Font()
+    >>> for glyphName, value in AGL2UV.items():
+    ...     font.newGlyph(glyphName)
+    ...     font[glyphName].unicodes = [value]
+    ...     font.newGlyph(glyphName + ".alt")
+    ...     font.newGlyph(glyphName + ".xxx")
+
+    >>> _search("e", font, False)
+    ('e', 'e')
+    >>> _search("eg", font, False)
+    ('eg', 'egrave')
+    >>> _search("e.", font, False)
+    ('e.', 'e.alt')
+
+    >>> _search("eight.al", font, True)
+    ('eight.al', 'eight')
+    """
+    # no text
+    if not text:
+        return text, None
+    glyphNames = font.keys()
+    match = None
+    # direct match
+    if text in glyphNames:
+        match = text
+    # character entry
+    elif len(text) == 1:
+        uniValue = ord(text)
+        match = font.unicodeData.glyphNameForUnicode(uniValue)
+        if match is not None:
+            text = ""
+    # fallback. find closest match
+    if match is None:
+        glyphNames = list(sorted(glyphNames))
+        if not deleting:
+            for glyphName in glyphNames:
+                if glyphName.startswith(text):
+                    match = glyphName
+                    break
+        else:
+            matches = []
+            for glyphName in glyphNames:
+                if text.startswith(glyphName):
+                    matches.append(glyphName)
+                elif match is not None:
+                    break
+            diff = None
+            for m in matches:
+                d = len(m) - len(text)
+                if diff is None or d < diff:
+                    match = m
+    return text, match
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
