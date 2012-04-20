03f2a3bcba1b4febe68a06da90ad71df1d79d293
tal <tal@9b05a68b-5939-0410-bc0c-8e10e763eb1c>
2012-04-20 18:30:29 +0000

    Typos.

--- Lib/defconAppKit/controls/fontInfoView.py
+++ Lib/defconAppKit/controls/fontInfoView.py
@@ -1440,7 +1440,7 @@ def postscriptStemSnapToUFO(string):
     except ValueError:
         values = []
     if len(values) >= 12:
-        value = value[:12]
+        values = values[:12]
     return values
 
 def infoListFromUFO(value):
