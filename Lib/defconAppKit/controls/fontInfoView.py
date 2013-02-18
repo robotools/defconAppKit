8cd3223bbffac6d585a15304bb607e08c7959b6d
tal <tal@9b05a68b-5939-0410-bc0c-8e10e763eb1c>
2013-02-18 13:57:57 +0000

    Patch from Frederik.

--- Lib/defconAppKit/controls/fontInfoView.py
+++ Lib/defconAppKit/controls/fontInfoView.py
@@ -1952,10 +1952,18 @@ postscriptWindowsCharacterSetOptions = [
     "OEM / DOS"
 ]
 
+def postscriptWindowsCharacterSetFromUFO(value):
+   return value - 1
+
+def postscriptWindowsCharacterSetToUFO(value):
+   return value + 1
+
 postscriptWindowsCharacterSetItem = inputItemDict(
-    title="Microsoft Character Set",
-    controlClass=vanilla.PopUpButton,
-    controlOptions=dict(items=postscriptWindowsCharacterSetOptions)
+   title="Microsoft Character Set",
+   controlClass=vanilla.PopUpButton,
+   controlOptions=dict(items=postscriptWindowsCharacterSetOptions),
+   conversionFromUFO=postscriptWindowsCharacterSetFromUFO,
+   conversionToUFO=postscriptWindowsCharacterSetToUFO
 )
 
 ## WOFF Identification
